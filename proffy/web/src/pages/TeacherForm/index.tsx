90d910e54874bb3c6a2db5f42ddb0bc879c22b50
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-08-03 11:53:48 -0300

    routes

---
+++ proffy/web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const TeacherForm: React.FC = () => {
+  return (
+    <h1>Teacher Form</h1>
+  );
+}
+
+export default TeacherForm;
