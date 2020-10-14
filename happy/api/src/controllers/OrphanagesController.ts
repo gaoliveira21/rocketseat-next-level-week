c6050bf757c6bc0e3a977764bd769eede36e0007
Gabriel <gaoliveira211999@gmail.com>
2020-10-14 16:26:44 -0300

    OrphanagesController file

---
+++ happy/api/src/controllers/OrphanagesController.ts
@@ -0,0 +1,32 @@
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+export default {
+  async create(request, response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
+  }
+}
