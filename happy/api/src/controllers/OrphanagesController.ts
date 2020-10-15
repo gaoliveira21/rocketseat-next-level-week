bd66b1390c06f65c594bdc6e5fcdbdd90ba84c8f
Gabriel <gaoliveira211999@gmail.com>
2020-10-15 13:29:01 -0300

    add views

--- happy/api/src/controllers/OrphanagesController.ts
+++ happy/api/src/controllers/OrphanagesController.ts
@@ -1,23 +1,27 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import orphanageView from '../views/orphanages_view';
+
 import Orphanage from '../models/Orphanage';
 
 export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
-    return response.json(orphanages);
+    return response.json(orphanageView.renderMany(orphanages));
   },
 
   async show(request: Request, response: Response) {
     const { id } = request.params;
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.findOneOrFail({ where: { id } });
+    const orphanage = await orphanagesRepository.findOneOrFail({ where: { id }, relations: ['images'] });
 
-    return response.json(orphanages);
+    return response.json(orphanageView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
