5062f9883d0167143d0b23f7c4e2fcd8ad78b53b
Gabriel <gaoliveira211999@gmail.com>
2020-10-15 11:44:01 -0300

    images upload

--- happy/api/src/routes.ts
+++ happy/api/src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-const routes = Router();
+import multer from 'multer';
+import uploadConfig from './config/upload';
 
 import OrphanagesController from './controllers/OrphanagesController';
 
-routes.post('/orphanages', OrphanagesController.create);
+const routes = Router();
+const uploads = multer(uploadConfig);
+
+routes.post('/orphanages', uploads.array('images'), OrphanagesController.create);
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 
