20e7cfd1fb10e30ab5f4cc7ce2120835643f67c8
Gabriel <gaoliveira211999@gmail.com>
2020-10-15 13:14:07 -0300

    persist images on create orphanages

--- happy/api/src/controllers/OrphanagesController.ts
+++ happy/api/src/controllers/OrphanagesController.ts
@@ -33,6 +33,12 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
+    const requestImages = request.files as Express.Multer.File[];
+
+    const images = requestImages.map(image => ({
+      path: image.filename
+    }))
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -40,7 +46,8 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends,
+      images
     });
 
     await orphanagesRepository.save(orphanage);
